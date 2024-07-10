8cf152de6f3a630775a172ce12ada8d16ee9dbde
Eshwar Lal <eshwarmalhi24@gmail.com>
2024-07-10 19:46:35 +0500

    Project Setup

---
+++ myportfolio/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h2>Portfolio Project......
+
+       </h2>
+   
+    </div>
+  );
+}
+
+export default App;
